bf9fc9a7e17f9fd02464f59f2e3740bf85892995
Myrthe Dullaart <myrtheluca@hotmail.com>
2024-04-25 17:03:27 +0200

    add header and left menu into seperate functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import initialEmails from './data/emails'
 import './styles/App.css'
 import EmailsList from './components/EmailsList'
 import ActiveEmail from './components/ActiveEmail'
+import Header from './components/Header'
+import LeftMenu from './components/LeftMenu'
 
 const getReadEmails = emails => emails.filter(email => !email.read)
 
@@ -27,50 +29,9 @@ function App() {
 
     return (
       <div className="app">
-        <header className="header">
-          <div className="left-menu">
-            <svg className="menu-icon" focusable="false" viewBox="0 0 24 24">
-              <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
-            </svg>
-  
-            <img
-              src="https://ssl.gstatic.com/ui/v1/icons/mail/rfr/logo_gmail_lockup_default_1x_r2.png"
-              alt="gmail logo"
-            />
-          </div>
-  
-          <div className="search">
-            <input className="search-bar" placeholder="Search mail" value={searchEmail} onChange={(e) => {setSearchEmail(e.target.value.toLocaleLowerCase())}}/>
-          </div>
-        </header>
-        <nav className="left-menu">
-          <ul className="inbox-list">
-            <li
-              className={`item ${currentTab === 'inbox' ? 'active' : ''}`}
-              onClick={() => setCurrentTab('inbox')}
-            >
-              <span className="label">Inbox</span>
-              <span className="count">{unreadEmails.length}</span>
-            </li>
-            <li
-              className={`item ${currentTab === 'starred' ? 'active' : ''}`}
-              onClick={() => setCurrentTab('starred')}
-            >
-              <span className="label">Starred</span>
-              <span className="count">{starredEmails.length}</span>
-            </li>
-  
-            <li className="item toggle">
-              <label htmlFor="hide-read">Hide read</label>
-              <input
-                id="hide-read"
-                type="checkbox"
-                checked={hideRead}
-                onChange={e => setHideRead(e.target.checked)}
-              />
-            </li>
-          </ul>
-        </nav>
+        <Header searchEmail={searchEmail} setSearchEmail={setSearchEmail} />
+        <LeftMenu currentTab={currentTab} setCurrentTab={setCurrentTab} unreadEmails={unreadEmails} starredEmails={starredEmails} hideRead={hideRead} setHideRead={setHideRead} />
+        
         { activeEmail === 'allEmails' &&
           <EmailsList filteredEmails={filteredEmails} setEmails={setEmails} setActiveEmail={setActiveEmail}/>
         }
